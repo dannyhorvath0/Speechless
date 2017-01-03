cc89a55dcd38369741ec24fd3622ce0c05ffed28
dannyhorvath0 <dannyhorvath0@gmail.com>
2017-01-03 20:48:25 +0100

    Fixed script
    Working when talking to the microphone the text is updated on the screen

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 var speechlessApp = angular.module('speechlessApp', []);
 //Create controller and inject Angular's $scope
 speechlessApp.controller('mainController', function($scope) {
-  if(annyang) {
-    //List of commands
-    var commands = {
-      '*tag': showText
+  if (annyang) {
+  // Let's define our first command. First the text we expect, and then the function it should call
+  var commands = {
+    '*text': function(text) {
+      $('#text').text(text);
     }
-
-    var showText = function(tag) {
-      $('#text').text(tag);
-    }
-    //Add commands to annyang
-    annyang.addCommands(commands);
-    //Set language to dutch
-    annyang.setLanguage("dutch");
-    //Start listening
-    annyang.start();
-  }
+  };
+  //Turn on debug
+  annyang.debug();
+  //Set language
+  annyang.setLanguage("nl-NL");
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start({ autoRestart: true, continuous: false });
+}
 });
