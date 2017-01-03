ea563813c307c39d967d62fb4d05454752731544
dannyhorvath0 <dannyhorvath0@gmail.com>
2017-01-03 18:53:42 +0100

    Created basic routing and added home template
    Created the basic routing for the homepage and added the template for
    the same page.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 //Create the module
-var speechlessApp = angular.module('speechlessApp', []);
+var speechlessApp = angular.module('speechlessApp', ['ngRoute']);
 
+speechlessApp.config(function($routeProvider) {
+
+  //Route for the homepage
+  .when('/', {
+    templateUrl : 'pages/home.html',
+    controller : 'mainController'
+  })
+});
 //Create controller and inject Angular's $scope
 speechlessApp.controller('mainController', function($scope) {
-  $scope.message = "Hello, world!";
 });
