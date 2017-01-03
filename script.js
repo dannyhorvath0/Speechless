e257c846c81875f63ebf0f9a62a6e85d2eafce7e
dannyhorvath0 <dannyhorvath0@gmail.com>
2017-01-03 21:16:06 +0100

    Created button for easier interaction and added a click event to it
    Created button for easier interaction and added a click event to it

--- script.js
+++ script.js
@@ -3,19 +3,29 @@ var speechlessApp = angular.module('speechlessApp', []);
 //Create controller and inject Angular's $scope
 speechlessApp.controller('mainController', function($scope) {
   if (annyang) {
-  // Let's define our first command. First the text we expect, and then the function it should call
-  var commands = {
-    '*text': function(text) {
-      $('#text').text(text);
-    }
+      // Let's define our first command. First the text we expect, and then the function it should call
+      var commands = {
+        '*text': function(text) {
+          $('#text').text(text);
+        }
+      };
+      //Turn on debug
+      annyang.debug();
+      //Set language
+      annyang.setLanguage("nl-NL");
+      // Add our commands to annyang
+      annyang.addCommands(commands);
+      // Start listening. You can call this here, or attach this call to an event, button, etc.
+      annyang.start({ autoRestart: false, paused: true, continuous: false });
   };
-  //Turn on debug
-  annyang.debug();
-  //Set language
-  annyang.setLanguage("nl-NL");
-  // Add our commands to annyang
-  annyang.addCommands(commands);
-  // Start listening. You can call this here, or attach this call to an event, button, etc.
-  annyang.start({ autoRestart: true, continuous: false });
-}
+
+  $scope.recordSpeech = function() {
+    if (annyang) {
+        if(annyang.isListening()) {
+          annyang.pause();
+        } else {
+          annyang.resume();
+        }
+    }
+  }
 });
