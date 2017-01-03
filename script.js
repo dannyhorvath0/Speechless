4aa2038c3281e9f0d74e08e3b48f0967434cff35
dannyhorvath0 <dannyhorvath0@gmail.com>
2017-01-03 19:52:35 +0100

    Cleaned up script.js and removed routing
    Removed routing since it will be a single page application

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 //Create the module
-var speechlessApp = angular.module('speechlessApp', ['ngRoute']);
-
-speechlessApp.config(function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl : 'pages/home.html',
-    controller : 'mainController'
-  })
-});
+var speechlessApp = angular.module('speechlessApp', []);
 //Create controller and inject Angular's $scope
 speechlessApp.controller('mainController', function($scope) {
+  if(annyang) {
+    //List of commands
+    var commands = {
+      '*tag': showText
+    }
+
+    var showText = function(tag) {
+      $('#text').text(tag);
+    }
+    //Add commands to annyang
+    annyang.addCommands(commands);
+    //Set language to dutch
+    annyang.setLanguage("dutch");
+    //Start listening
+    annyang.start();
+  }
 });
