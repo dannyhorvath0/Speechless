c18b64c7292b0df4d1464d68f47fafbe31ece3f9
luukmaters <luukmaters@gmail.com>
2017-01-18 11:57:32 +0100

    Backend initial commit

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+
+
+var exec = require('child_process').exec;
+var parser = require('xml2json');
+
+/*
+   $ALPINO_HOME/bin/Alpino -notk -veryfast user_max=20000   server_kind=parse   server_port=11211   assume_input_is_tokenized=on   debug=0   -init_dict_p   batch_command=alpino_server
+*/
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+        res.render('index', { title: "test" });
+});
+
+router.get('/text?*', function(req, res, next) {
+    console.log(req.query.t);
+    exec('echo "'+req.query.t+'" | nc 127.0.0.1 11211', function(error, stdout, stderr) {
+        /*
+            { woorden: [{word: "geef", pt: "WW", pos: "verb"}, {word: "alle", pt: "vnw", pos: "det"},  {word: "klanten", pt: "n", pos: "noun"}]}
+        */
+        var json = JSON.parse(parser.toJson(stdout));
+        console.log(JSON.stringify(json, null, 4));
+        res.send({ "words": makeArray(json.alpino_ds, json.alpino_ds.node.end, [])});
+    });
+});
+
+function makeArray(json, nWords, resArr){
+    if(json.node == undefined){
+        if(json.word != undefined){
+            var obj = {word: json.word, pt: json.pt, pos: json.pos};
+            resArr.push(obj);
+        }
+        if(resArr.length == nWords){
+            return resArr;
+        }
+    }else{
+        if(json.node[0] != undefined){
+            return loopNodeArr(json.node, nWords, resArr);
+        }else{
+            return makeArray(json.node, nWords, resArr);
+        }
+    }
+}
+
+function loopNodeArr(json, nWords, resArr){
+    for(var i in json){
+        makeArray(json[i], nWords, resArr);
+    }
+    if(resArr.length == nWords){
+        return resArr;
+    }
+}
+
+
+
+module.exports = router;
